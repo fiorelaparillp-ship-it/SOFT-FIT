72226a78998754ba62d4ade9ba79dd736eb7cdbf
FIORELA <fiorelaparillp@gmail.com>
2026-07-30 19:49:16 -0500

    Agregado favicon de SOFT-FIT

--- login.php
+++ login.php
@@ -61,6 +61,10 @@ $_SESSION['rol'] = $fila['rol'];
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <title>Login SOFT-FIT</title>
+
+<link rel="icon" type="image/png" href="img/logonav.png">
+
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 <link rel="stylesheet" href="css/global.css">
 <style>
